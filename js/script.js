68ce49f8eabcc667dd4e5ff86a59299c42b96ab4
Kenz <75763587+kora-codes@users.noreply.github.com>
2021-07-07 00:36:31 -0400

    positioning fix
    images are now centered

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
 let generate_btn =  document.querySelector(".generate_btn");
-
 generate_btn.addEventListener("click", fetchPics);
 
 function fetchPics() {
-
-fetch('https://api.thecatapi.com/v1/images/search')
-    .then(response => response.json())
-    .then((data) => {
-        let catsImgUrl = data[0].url
-
-        let catsImgElement = document.createElement("img")
-        catsImgElement.setAttribute('src', `${catsImgUrl}`)
-        catsImgElement.classList.add("showcase")
-
-        let catsImg = document.querySelector(".catsImg")
-        catsImg.appendChild(catsImgElement)
-
-    })
-    .catch((err => console.log(err)))
+    fetch('https://api.thecatapi.com/v1/images/search')
+        .then(response => response.json())
+        .then((data) => {
+            let catsImgUrl = data[0].url;
+            let catsImgElement = document.createElement("img");
+            catsImgElement.onload = () => generate_btn.textContent = "Show more...";
+            catsImgElement.setAttribute('src', `${catsImgUrl}`);
+            catsImgElement.classList.add("showcase");
+            let catsImg = document.querySelector(".catsImg");
+            catsImg.appendChild(catsImgElement);
+        })
+        .catch(console.log)
 };
